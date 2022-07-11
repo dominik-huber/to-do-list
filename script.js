19c96a04233e9cf755fd71529bc7e2693b64a487
dominik-huber <strangeman94@gmx.de>
2022-07-11 16:38:00 +0200

    add files

---
+++ script.js
@@ -0,0 +1,13 @@
+// Here starts the codes for section 1
+
+// Here end the codes for section 1
+
+
+// Here starts the codes for section 2
+
+// Here end the codes for section 2
+
+
+// Here starts the codes for section 3
+
+// Here end the codes for section 3
