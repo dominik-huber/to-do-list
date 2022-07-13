d5ac609b888d7e7cc57d093be995b6c84a2b22d2
dominik-huber <strangeman94@gmx.de>
2022-07-13 10:04:56 +0200

    Debug mark as done

--- script.js
+++ script.js
@@ -12,25 +12,21 @@
 const taskList = document.querySelector('.taskList');
 const tasks = document.querySelectorAll('.form-check-input');
 
-tasks.forEach(el => {
-  el.addEventListener('click', (e) => {
+tasks.forEach(task => {
+  task.addEventListener('click', (e) => {
     e.currentTarget.parentElement.parentElement.classList.toggle('disable');
+    e.currentTarget.checked = false;
 
 		// Toggle class "done" of the clicked task
 		const taskDisabled = e.currentTarget.closest(".task")
 		taskDisabled.classList.toggle('done');
 
 		// Check if task has class done and give position
-		if (taskDisabled.classList.contains("done")) {
-			taskList.appendChild(taskDisabled);
-		} else {
-			taskList.prepend(taskDisabled);
-		}
+		(taskDisabled.classList.contains("done"))
+    ? taskList.append(taskDisabled)
+    : taskList.prepend(taskDisabled);
 
   });
 
 });
-
-
-
 // Here end the codes for mark task as done
