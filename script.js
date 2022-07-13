a62d10cd8bda7f47c3f8d9233b8c95da25a6d9a7
dominik-huber <strangeman94@gmx.de>
2022-07-13 16:08:31 +0200

    debug const

--- script.js
+++ script.js
@@ -6,12 +6,9 @@ const tasks = document.querySelectorAll('.form-check-input');
 
 
 // Here starts the codes for mark task as done
-const taskList = document.querySelector('.taskList');
-const tasks = document.querySelectorAll('.form-check-input');
-
 tasks.forEach(task => {
   task.addEventListener('click', (e) => {
-    // Toggle Class "disable" of the parent div of the task after clicking the checkbox 
+    // Toggle Class "disable" of the parent div of the task after clicking the checkbox
     // and automatically uncheck the checkbox
     e.currentTarget.parentElement.parentElement.classList.toggle('disable');
     e.currentTarget.checked = false;
