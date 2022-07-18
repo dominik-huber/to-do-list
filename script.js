1c9b5d686400d3ae87e3cbf5d4d27d69bc22d310
Gabriela <g.negran@yahoo.com>
2022-07-18 17:45:39 +0200

    added IDs, made the functions work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // from Google
-function hashCode(str) {
+/*function hashCode(str) {
   var hash = 0, i, chr;
   if (str.length === 0) return hash;
   for (i = 0; i < str.length; i++) {
@@ -9,10 +9,11 @@ function hashCode(str) {
   }
   return hash;
 };
-
+*/
 const taskList = document.querySelector('.taskList');
 const tasks = document.querySelectorAll('.form-check-input');
 const buttonlist = document.querySelectorAll('.delete_button');
+let tasknumber = 1;
 //Goal: I click on the button and a new todo gets added
 //1- retrieve the button and save it into a variable
 const addTaskBtn = document.querySelector('#addTaskBtn');
@@ -24,60 +25,59 @@ addTaskBtn.addEventListener('click', (e) => {
   const taskName = document.querySelector('#taskName').value;
   // 3.2 create an element and store it into a variable
   const element = document.createElement('div');
-  const id = hashCode(taskName) +'';
-  element.setAttribute('id',id);
+  /*const id = hashCode(taskName) +'';
+  element.setAttribute('id',id);*/
   element.classList.add('task','mt-3');
+  
   // 3.3 add the value of the task as innerText to the newly create element
   element.innerHTML = `          
   <div class="row justify-content-center flex-row flex-wrap align-items-center">
     <div class="col-9">
       <div class="input-group">
         <div class="input-group-text">
-          <input class="form-check-input mt-0" type="checkbox" value=""
+          <input class="form-check-input mt-0" type="checkbox" value="" id="${'markTask' + tasknumber }"
             aria-label="Checkbox for following text input" />
         </div>
-        <input type="text" class="form-control" aria-label="Text input with checkbox" placeholder="${taskName}"
+        <input type="text" class="form-control" aria-label="Text input with checkbox" placeholder="${taskName}" id="${'taskText' + tasknumber}"
           disabled />
       </div>
     </div>
     <div class="col-3">
-      <button class="btn btn-primary" id="editTask">
+      <button class="btn btn-primary" id="${'editTask' + tasknumber }">
         <i class="bi-pencil-square"></i>
       </button>
-      <button class="btn btn-primary delete_button ms-3" id="deleteTask">
+      <button class="btn btn-primary delete_button ms-3" id="${'deleteTask' + tasknumber}">
         <i class="bi-trash"></i>
       </button>
     </div>
   </div>`;
   // 3.4 target the spot in the html where you want to inject the newly created element
   taskList.prepend(element);
-  markTask();
-  deleteTask();
-  editTask()
+  document.getElementById('markTask' + tasknumber ).addEventListener("click", markTask);
+  document.getElementById('deleteTask' + tasknumber).addEventListener("click", deleteTask);
+  document.getElementById('editTask' + tasknumber ).addEventListener("click", editTask);
+  tasknumber++;
 })
 
-/*
-add icons for delete button in html
-extract delete button from DOM and store them in a variable
-*/
+
 
 function deleteTask(){
-  buttonlist.forEach(button => {
+  /*buttonlist.forEach(button => {
     button.addEventListener('click', (e) =>
       e.currentTarget.parentElement.remove()
     )
-  });
-  
+  });*/
+  this.parentElement.parentElement.parentElement.remove()
 }
 
 // Here starts the codes for mark task as done
 function markTask(){
-  tasks.forEach(task => {
-    task.addEventListener('click', (e) => {
+  /*tasks.forEach(task => {
+    task.addEventListener('click', (e) => {*/
       // Toggle Class "disable" of the parent div of the task after clicking the checkbox
       // and automatically uncheck the checkbox
-      e.currentTarget.parentElement.parentElement.classList.toggle('disable');
-      e.currentTarget.checked = false;
+      this.parentElement.parentElement.classList.toggle('disable');
+      this.checked = false;
   
       // Toggle class "done" of the clicked task
       const taskDisabled = e.currentTarget.closest(".task")
@@ -87,27 +87,23 @@ function markTask(){
       (taskDisabled.classList.contains("done"))
         ? taskList.append(taskDisabled)
         : taskList.prepend(taskDisabled);
-    });
-  });
+    //});
+  //});
 }
 // Here end the codes for mark task as done
 
 function editTask(){
-  var buttons = document.getElementsByClassName('editButton');
-  console.log(buttons);
-  var edit = function () {
-      var taskID = this.id.charAt(this.id.length - 1);
-      var taskInput = document.getElementById("taskText" + taskID);
-      if (taskInput.disabled) {
-          taskInput.removeAttribute("disabled");
-      } else {
-          taskInput.setAttribute("disabled", "true");
-          taskInput.focus();
-      }
-      
-  }
-  for (var i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener('click', edit, false);
+  let taskID = this.id.charAt(this.id.length - 1);
+  let taskInput = document.getElementById("taskText" + taskID);
+  let taskValue = taskInput.value;
+  console.log("test")
+
+  if (taskInput.disabled) {
+      taskInput.removeAttribute("disabled");
+      taskInput.focus();
+  } else {
+      taskInput.setAttribute("disabled", "true");
+      taskInput.setAttribute('value',taskValue);
   }
 }
 
