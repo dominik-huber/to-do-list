5c7586249b537c1d298f66c1dff4192a610c40b1
ckaule <ckaule@fl1rt.net>
2022-07-19 22:46:44 +0200

    read and set markTask isDone

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
-// from Google
-/*function hashCode(str) {
-  var hash = 0, i, chr;
-  if (str.length === 0) return hash;
-  for (i = 0; i < str.length; i++) {
-    chr   = str.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
-};
-*/
-
-
 const taskList = document.querySelector('.taskList');
 const tasks = document.querySelectorAll('.form-check-input');
 const buttonlist = document.querySelectorAll('.delete_button');
 let tasknumber = 1;
+var shallow = 'shallow is cool';
+
 //Goal: I click on the button and a new todo gets added
 //1- retrieve the button and save it into a variable
 const addTaskBtn = document.querySelector('#addTaskBtn');
 
-if (!task == "") {
-  
-}
+// if (!task == "") {
+// }
 
 //2- attach event (click) to the button
 addTaskBtn.addEventListener('click', (e) => {
-
   // 3.1 retrieve the value of the text typed inside the input and save it into a variable
   const taskName = document.querySelector('#taskName').value;
 
+  //creeate task object and save it to local storage
   const task = {
     name: taskName,
     isDone: false,
     id: tasknumber
   }
+  window.localStorage.setItem('task' + tasknumber, JSON.stringify(task));
 
-  window.localStorage.setItem('task'+tasknumber, JSON.stringify(task));
-  window.localStorage.getItem('task'+tasknumber);
-//console.log(window.localStorage.getItem('task'+tasknumber));
-
-  //3- write the event handler
+  //3- event handler
   // 3.2 create an element and store it into a variable
   const element = document.createElement('div');
   /*const id = hashCode(taskName) +'';
   element.setAttribute('id',id);*/
-  element.classList.add('task','mt-3');
+  element.classList.add('task', 'mt-3');
 
   // 3.3 add the value of the task as innerText to the newly create element
   element.innerHTML = `
@@ -53,15 +37,15 @@ addTaskBtn.addEventListener('click', (e) => {
     <div class="col-9">
       <div class="input-group">
         <div class="input-group-text">
-          <input class="form-check-input mt-0" type="checkbox" value="" id="${'markTask' + tasknumber }"
+          <input class="form-check-input mt-0" type="checkbox" data-taskNumber="${tasknumber}" value="" id="${'markTask' + tasknumber}"
             aria-label="Checkbox for following text input" />
         </div>
         <input type="text" class="form-control" aria-label="Text input with checkbox" placeholder="${taskName}" id="${'taskText' + tasknumber}"
           disabled />
       </div>
     </div>
-    <div class="col-3">
-      <button class="btn btn-primary" id="${'editTask' + tasknumber }">
+    <div class="col-3 d-flex">
+      <button class="btn btn-primary" id="${'editTask' + tasknumber}">
         <i class="bi-pencil-square"></i>
       </button>
       <button class="btn btn-primary delete_button ms-3" id="${'deleteTask' + tasknumber}">
@@ -71,55 +55,60 @@ addTaskBtn.addEventListener('click', (e) => {
   </div>`;
   // 3.4 target the spot in the html where you want to inject the newly created element
   taskList.prepend(element);
-  document.getElementById('markTask' + tasknumber ).addEventListener("click", markTask);
+
+  //attach eventlisteners to all buttons and call the functions
+  document.getElementById('markTask' + tasknumber).addEventListener("click", function () {
+    markTask(this)
+  });
   document.getElementById('deleteTask' + tasknumber).addEventListener("click", deleteTask);
-  document.getElementById('editTask' + tasknumber ).addEventListener("click", editTask);
+  document.getElementById('editTask' + tasknumber).addEventListener("click", editTask);
+  //icnrease tasknumber for every new task id
   tasknumber++;
 })
 
-
-
-function deleteTask(){
-  /*buttonlist.forEach(button => {
-    button.addEventListener('click', (e) =>
-      e.currentTarget.parentElement.remove()
-    )
-  });*/
+//delete the task clicked
+function deleteTask() {
   this.parentElement.parentElement.parentElement.remove()
 }
 
-// Here starts the codes for mark task as done
-function markTask(){
-  /*tasks.forEach(task => {
-    task.addEventListener('click', (e) => {*/
-      // Toggle Class "disable" of the parent div of the task after clicking the checkbox
-      // and automatically uncheck the checkbox
-      this.parentElement.parentElement.classList.toggle('disable');
-      this.checked = false;
-
-      // Toggle class "done" of the clicked task
-      const taskDisabled = this.closest(".task")
-      taskDisabled.classList.toggle('done');
-
-      // Check if task has class done and give position
-      (taskDisabled.classList.contains("done"))
-        ? taskList.append(taskDisabled)
-        : taskList.prepend(taskDisabled);
-    //});
-  //});
+//mark the task clicked as done
+function markTask(button) {
+  // Toggle Class "disable" of the parent div of the task after clicking the checkbox
+  // and automatically uncheck the checkbox
+  button.parentElement.parentElement.classList.toggle('disable');
+  // Toggle class "done" of the clicked task
+  const taskDisabled = button.closest(".task")
+  taskDisabled.classList.toggle('done');
+  
+  // get current task out of local storage
+  const currentTask = window.localStorage.getItem('task' + button.dataset.tasknumber);
+  const currenTaskParsed = JSON.parse(currentTask);
+  
+  // Check if task has class done and give position and change the value of isDone
+  if (taskDisabled.classList.contains("done")) {
+    taskList.append(taskDisabled)
+    currenTaskParsed.isDone = true;
+  }
+  else {
+    taskList.prepend(taskDisabled);
+    currenTaskParsed.isDone = false;
+  }
+  // save the task back to local storage
+  const saveTask = JSON.stringify(currenTaskParsed);
+  localStorage.setItem("task" + button.dataset.tasknumber, saveTask);
 }
-// Here end the codes for mark task as done
 
-function editTask(){
+// edit the task clicked
+function editTask() {
   let taskID = this.id.charAt(this.id.length - 1);
   let taskInput = document.getElementById("taskText" + taskID);
   let taskValue = taskInput.value;
 
   if (taskInput.disabled) {
-      taskInput.removeAttribute("disabled");
-      taskInput.focus();
+    taskInput.removeAttribute("disabled");
+    taskInput.focus();
   } else {
-      taskInput.setAttribute("disabled", "true");
-      taskInput.setAttribute('value',taskValue);
+    taskInput.setAttribute("disabled", "true");
+    taskInput.setAttribute('value', taskValue);
   }
 }
